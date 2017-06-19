cdcfaedba582a352fc94574dcb9c0f89ed70cf85
DevLin <617060316@qq.com>
2017-06-19 17:21:11 +0800

    new project thinkjs-rabc 	new file:   sql/think-rabc.sql 	new file:   src/common/bootstrap/global.js 	new file:   src/common/bootstrap/middleware.js 	new file:   src/common/config/config.js 	new file:   src/common/config/db.js 	new file:   src/common/config/env/development.js 	new file:   src/common/config/env/production.js 	new file:   src/common/config/env/testing.js 	new file:   src/common/config/error.js 	new file:   src/common/config/hook.js 	new file:   src/common/config/locale/en.js 	new file:   src/common/config/session.js 	new file:   src/common/config/view.js 	new file:   src/common/controller/error.js 	new file:   src/home/config/config.js 	new file:   src/home/controller/base.js 	new file:   src/home/controller/index.js 	new file:   src/home/logic/index.js 	new file:   src/home/model/index.js 	new file:   view/common/error_400.html 	new file:   view/common/error_403.html 	new file:   view/common/error_404.html 	new file:   view/common/error_500.html 	new file:   view/common/error_503.html 	new file:   view/home/index_index.html 	new file:   www/README.md 	new file:   www/development.js 	new file:   www/production.js 	new file:   www/testing.js

---
+++ sql/think-rabc.sql
@@ -0,0 +1,60 @@
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rabc` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `rabc`;
+
+/*Table structure for table `north_admin` */
+
+DROP TABLE IF EXISTS `rabc_user`;
+
+CREATE TABLE `rabc_user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT  COMMENT '主键，自增',
+  `code` VARCHAR(32) NOT NULL UNIQUE COMMENT '用户编码',
+  `username` VARCHAR(16) NOT NULL COMMENT '用户名称',
+  `password` VARCHAR(48) NOT NULL COMMENT '用户密码(md5加密)',
+  `status` INT(1) NOT NULL DEFAULT '1' COMMENT '是否启用(0:不启用 1:启用)',
+  `create_time` INT(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `login_time` INT(11) NOT NULL DEFAULT '0' COMMENT '登陆时间',
+  `login_ip` VARCHAR(16) NOT NULL DEFAULT '0.0.0.0' COMMENT '登陆IP',
+  `delstatus` TINYINT  NOT NULL DEFAULT '1' COMMENT '是否删除(0:删除 1:未删除)',
+  PRIMARY KEY (`id`)
+) ENGINE=MYISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `rabc_menu`;
+CREATE TABLE `rabc_menu` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT  COMMENT '主键，自增',
+  `pid` INT(11)  NOT NULL DEFAULT 0 COMMENT  '父键',
+  `code` VARCHAR(32) NOT NULL UNIQUE COMMENT '菜单编码',
+  `url` VARCHAR(255) NULL COMMENT '菜单地址',
+  `ismenu` TINYINT NOT NULL COMMENT '是否菜单(0:菜单 1:操作权限)',
+  `name` VARCHAR(16) NOT NULL COMMENT '菜单名称',
+  `briefname` VARCHAR(48) NOT NULL COMMENT '菜单简称',
+  `status` INT(1) NOT NULL DEFAULT '1' COMMENT '是否启用(0:不启用 1:启用)',
+  `create_time` INT(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `delstatus` TINYINT  NOT NULL DEFAULT '1' COMMENT '是否删除(0:删除 1:未删除)',
+  PRIMARY KEY (`id`)
+) ENGINE=MYISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `rabc_group`;
+CREATE TABLE `rabc_group` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT  COMMENT '主键，自增',
+  `pid` INT(11)  NOT NULL DEFAULT 0 COMMENT  '父键',
+  `code` VARCHAR(32) NOT NULL UNIQUE COMMENT '组编码',
+  `name` VARCHAR(16) NOT NULL COMMENT '组名称',
+  `briefname` VARCHAR(48) NOT NULL COMMENT '组简称',
+  `status` INT(1) NOT NULL DEFAULT '1' COMMENT '是否启用(0:不启用 1:启用)',
+  `create_time` INT(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `delstatus` TINYINT  NOT NULL DEFAULT '1' COMMENT '是否删除(0:删除 1:未删除)',
+  PRIMARY KEY (`id`)
+) ENGINE=MYISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
